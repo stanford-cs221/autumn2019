56b9d35ce8b8958fc5782b2ae696abb41e826f6b
rpryzant <rpryzant@stanford.edu>
2019-11-20 19:42:24 -0800

    project list update

--- 2018/project-list.js
+++ 2018/project-list.js
@@ -11,16 +11,16 @@ $(function () {
     var titleDiv = $('<div>').addClass('titlebar').append($('<a>').addClass('title')).append(project.title); //.attr('href', 'restricted/posters/' + project.user + '/poster.pdf'));
     projectDiv.append(titleDiv);
 
-    // var sessionDiv = $('<div>').addClass('session').append($('<span>').addClass('author').append(
-    //   project.session));
-    // buttons.append(sessionDiv);
+    var sessionDiv = $('<div>').addClass('session').append($('<span>').addClass('author').append(
+      project.session));
+    buttons.append(sessionDiv);
 
-    // var authorDiv = $('<div>').addClass('authorbar').append($('<span>').addClass('author').append(
-    //   project.group.map(function(member) { return member.name; }).join(', ')));
-    // projectDiv.append(authorDiv);
+    var authorDiv = $('<div>').addClass('authorbar').append($('<span>').addClass('author').append(
+      project.group.map(function(member) { return member.name; }).join(', ')));
+    projectDiv.append(authorDiv);
 
-    // var mentorDiv = $('<div>').addClass('mentorbar').append($('<span>').addClass('mentor').append(
-    //   'Mentor: ' + project.mentor.name));
-    // projectDiv.append(mentorDiv);
+    var mentorDiv = $('<div>').addClass('mentorbar').append($('<span>').addClass('mentor').append(
+      'Mentor: ' + project.mentor.name));
+    projectDiv.append(mentorDiv);
   }
 });
